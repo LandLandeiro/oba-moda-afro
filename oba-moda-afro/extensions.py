9071141f60be5245487e2f8166ec0ad7f23a8bff
Marllon Machado <43810820+krocksss@users.noreply.github.com>
2025-11-12 14:01:52 -0300

    Add Flask-Migrate for database migrations

--- oba-moda-afro/extensions.py
+++ oba-moda-afro/extensions.py
@@ -2,10 +2,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
+migrate = Migrate()
 
 #config de login e verificação
 login_manager.login_view = 'login'
